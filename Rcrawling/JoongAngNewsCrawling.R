129820d007b8bbe43456e16ce4ea0bd1d86f5e1d
Hyun kyoung OH <ohg147@naver.com>
2019-09-23 17:29:45 +0900

    news

--- Rcrawling/JoongAngNewsCrawling.R
+++ Rcrawling/JoongAngNewsCrawling.R
@@ -72,37 +72,37 @@ for(categorynb in 2:5){
       contentLink<-remDr$findElements(using='css',contentaddr)
       content<-unlist(sapply(contentLink,function(x){x$getElementText()}))
       
-      #이미지텍스트
-      imgtextaddr<-'#article_body > div.ab_photo'
-      imgtextLink<-remDr$findElements(using='css',imgtextaddr)
-      if(length(imgtextLink)!=0){
-        imgtext<-unlist(sapply(imgtextLink,function(x){x$getElementText()}))
-        
-        imgtext <- gsub("\\[","",gsub("]","",imgtext))
-        imgtext <- gsub("[()]","",imgtext)
-        
-        # #사진 주석 제외처리
-        content <- gsub("\\[","",gsub("]","",content))
-        content<- gsub("[()]","",content)
-        
-        j <- 1
-        repeat{
-          content<-str_replace(content,imgtext[j],"")
-          
-          if(j==length(imgtext)){
-            break
-          }
-          j <- j+1
-        }
-      }
-      #영상텍스트
-      videoaddr<-'#article_body > div:nth-child(16) > div'
-      videoLink<-remDr$findElements(using='css',videoaddr)
-      if(length(videoLink)!=0){
-        videotext<-unlist(sapply(videoLink,function(x){x$getElementText()}))
-        content<-str_replace(content,videotext,"")
-      }
-      
+      # #이미지텍스트
+      # imgtextaddr<-'#article_body > div.ab_photo'
+      # imgtextLink<-remDr$findElements(using='css',imgtextaddr)
+      # if(length(imgtextLink)!=0){
+      #   imgtext<-unlist(sapply(imgtextLink,function(x){x$getElementText()}))
+      #   
+      #   imgtext <- gsub("\\[","",gsub("]","",imgtext))
+      #   imgtext <- gsub("[()]","",imgtext)
+      #   
+      #   # #사진 주석 제외처리
+      #   content <- gsub("\\[","",gsub("]","",content))
+      #   content<- gsub("[()]","",content)
+      #   
+      #   j <- 1
+      #   repeat{
+      #     content<-str_replace(content,imgtext[j],"")
+      #     
+      #     if(j==length(imgtext)){
+      #       break
+      #     }
+      #     j <- j+1
+      #   }
+      # }
+      # #영상텍스트
+      # videoaddr<-'#article_body > div:nth-child(16) > div'
+      # videoLink<-remDr$findElements(using='css',videoaddr)
+      # if(length(videoLink)!=0){
+      #   videotext<-unlist(sapply(videoLink,function(x){x$getElementText()}))
+      #   content<-str_replace(content,videotext,"")
+      # }
+      # 
       if(categorynb==2){
         politics<-c(politics,title)
         politics<-c(politics,date)
@@ -123,7 +123,7 @@ for(categorynb in 2:5){
       
       print(title)
       remDr$goBack()
-      Sys.sleep(3)
+      Sys.sleep(5)
     }
     if(endFlag)
       break
