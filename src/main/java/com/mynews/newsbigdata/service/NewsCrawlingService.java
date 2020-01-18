016b199de77b4d73b753a73a640c45a83c9b9a5a
ysgo <wjion8428@gmail.com>
2020-01-18 17:46:51 +0900

    remove R dependencies and code

--- src/main/java/com/mynews/newsbigdata/service/NewsCrawlingService.java
+++ src/main/java/com/mynews/newsbigdata/service/NewsCrawlingService.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import org.rosuda.REngine.Rserve.RConnection;
-import org.rosuda.REngine.Rserve.RserveException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.mynews.newsbigdata.model.NewsVO;
@@ -25,13 +22,10 @@ public class NewsCrawlingService {
 	@Autowired
 	private CheckInsertService checkInsertService;
 	
-//	@Scheduled(cron="0 0/50 9-18 * * *")	// 9시 ~ 18시까지 50분 간격으로 실행
-//	@Scheduled(fixedDelay=180000)	// 시작 후 30분 뒤부터 시작
 	public void scheduleRun() {
 		String accessKey = env.getProperty("etri.KEY"); // 발급받은 API Key
 		String newsFilePath = env.getProperty("newsFile.url");
 		checkInsertService.checkZone();
-		crawlingNews(newsFilePath);
 		List<NewsVO> list = loadNewsData(newsFilePath);
 		// 수집 데이터 분석 저장 | 예외 처리 필요 
 		analysisService.runNewsAnalysis(list, accessKey);
@@ -54,26 +48,4 @@ public class NewsCrawlingService {
 		return list;
 	}
 	
-	public void crawlingNews(String newsFilePath) {
-		RConnection rc = null;
-		try {
-			rc = new RConnection();
-			rc.eval("driverClass <- '" + env.getProperty("rdb.class") + "'");
-			rc.eval("connectPath <- '" + env.getProperty("mysql-connector.url") + "'");
-			rc.eval("driver <- '" + env.getProperty("rdb.driver") + "'");
-			rc.eval("userName <- '" + env.getProperty("db.userName") + "'");
-			rc.eval("password <- '" + env.getProperty("db.password") + "'");
-			rc.eval("seleniumIP <- '" + env.getProperty("selenium.ip") + "'");
-			rc.eval("seleniumPort <- " + env.getProperty("selenium.port"));
-			rc.eval("seleniumBrowser <- '" + env.getProperty("selenium.browser") + "'");
-			rc.eval("path <- '" + newsFilePath + "'");
-			rc.eval("source('" + env.getProperty("r.url") + "')");
-		} catch (RserveException e) {
-			System.out.println("Failed Rserve Server! Check the Rserve Server");
-			System.out.println("But now I will ignore the error due to the problem of language support!");
-		} finally {
-			if (rc != null)
-				rc.close();
-		}
-	}
 }
